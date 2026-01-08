4130e86ab998c53c684eaa94ba0bd1c3530a57bc
Pranjal939 <pranjaljotangiya191@gmail.com>
2026-01-08 13:29:14 +0530

    modified file with username

--- demo.py
+++ demo.py
@@ -6,3 +6,6 @@ print(sklearn.__version__)
 import numpy
 #virctual environment 
 #python ~m venv en #type in terminal 
+username = input("enter username:")
+marks = float("enter maeks:")
+print(f'welcome {username},marks :%0.2f' %marks)
