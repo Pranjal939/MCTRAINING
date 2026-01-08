38430df7dbe0ea3c51b83bea11719173c9ed2000
Pranjal939 <pranjaljotangiya191@gmail.com>
2026-01-08 11:36:20 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,7 @@
+print('this is deom spython script')
+import numpy as np
+print('numpy version:',np.__version__)
+import sklearn
+print(sklearn.__version__)
+#virctual environment 
+#python ~m venv en #type in terminal 
