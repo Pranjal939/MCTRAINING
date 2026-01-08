77e0df14d9334911dc7381cf2b6f0d319f70f56e
Pranjal939 <pranjaljotangiya191@gmail.com>
2026-01-08 14:36:13 +0530

    modified

--- demo.py
+++ demo.py
@@ -1,11 +1,17 @@
-print('this is deom spython script')
-import numpy as np
-print('numpy version:',np.__version__)
-import sklearn
-print(sklearn.__version__)
-import numpy
+#print('this is deom spython script')
+#import numpy as np
+#print('numpy version:',np.__version__)pranjal
+#import sklearn
+#print(sklearn.__version__)
+#import numpy
 #virctual environment 
 #python ~m venv en #type in terminal 
 username = input("enter username:")
-marks = float("enter maeks:")
-print(f'welcome {username},marks :%0.2f' %marks)
+marks = float(input("enter marks:"))
+print(f'welcome {username},marks :%5.4d' %marks)
+
+s1 = 'my string'
+s2 = '2nd line'
+print(s1,end='@')
+print(end = '@')
+print(s2)
