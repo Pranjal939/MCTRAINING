a61de6404e80c1adbe09d3d263b7f16bd10dcced
Pranjal939 <pranjaljotangiya191@gmail.com>
2026-01-08 15:52:05 +0530

    modified

--- demo.py
+++ demo.py
@@ -6,14 +6,22 @@
 #import numpy
 #virctual environment 
 #python ~m venv en #type in terminal 
-username = input("enter username:")
-marks = float(input("enter marks:"))
-print(f'welcome {username},marks :%5.4d' %marks)
+#username = input("enter username:")
+#marks = float(input("enter marks:"))
+#print(f'welcome {username},marks :%5.4d' %marks)
 
-s1 = 'GeeksforGeeks'
-s2 = ' 2nd line'
-print(s1[::-1],end='@')
-print(end = '@')
-list = s2.split()
-s3 = "".join([i.capitalize() for i in list])
-print(s3)
+#s1 = 'GeeksforGeeks'
+#s2 = ' 2nd line'
+#print(s1[::-1],end='@')
+#print(end = '@')
+#list = s2.split()
+#s3 = "".join([i.capitalize() for i in list])
+#print(s3)
+
+#lst = [1,10,'abc',5,6]
+#print(lst)
+#print(lst.pop())
+
+products = {'laptop' :800,'mouse':200}
+print(products.update({'monitor' : 1000}))
+print(products)
