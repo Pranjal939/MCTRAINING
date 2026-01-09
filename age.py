4526558936b1d53355d03bccfea0365eae0533e0
Pranjal939 <pranjaljotangiya191@gmail.com>
2026-01-09 12:55:00 +0530

    first commit

---
+++ age.py
@@ -0,0 +1,9 @@
+age =  int(input("enter a age"))
+if age < 12 :
+    print("they are the child")
+elif age < 19:
+    print("they are teenager")
+elif age < 64:
+    print("they are an adult") 
+else :
+    print("they are senior")
