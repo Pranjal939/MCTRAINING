fc37e1ec659e5720ee2cbc6b0f01cc94677be54a
Pranjal939 <pranjaljotangiya191@gmail.com>
2026-01-09 15:09:36 +0530

    first commit

---
+++ cls.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, n, a, g):
+        self.name = n
+        self.age = a
+        self.gender = g
+
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}, Gender: {self.gender}")
+
+    def is_adult(self):
+        return self.age >= 18
+
+p1 = Person("Pranjal", 19, "Female")
+p1.display_info()
+print("Is adult:", p1.is_adult())
