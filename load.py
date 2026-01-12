7515d14d6eddfc152fd3856ff3410410a686a3ea
Pranjal939 <pranjaljotangiya191@gmail.com>
2026-01-12 14:27:47 +0530

    commit

--- load.py
+++ load.py
@@ -4,5 +4,12 @@ data = pd.read_csv('products.csv')
 data.head()
 data.info()
 st.write(data['Price'])
-st.title('upload your data in')
-
+st.title('upload your data in csv format')
+file = st.file_uploader('choose a file',type='csv')
+if file is not None:
+    df = pd.read_csv(file)
+    st.dataframe(df)
+    c = st.selectbox('select column for static',df.columns)
+    st.line_chart(df[c])
+else:
+    st.error('please upload a csv file')
