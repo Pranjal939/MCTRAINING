89808ecefad8ba7488b6899116c2c2f23afe675b
Pranjal939 <pranjaljotangiya191@gmail.com>
2026-01-13 11:41:59 +0530

    first commit

--- app2.py
+++ app2.py
@@ -7,8 +7,16 @@ data2=[10,20,30,40,50]
 
 import matplotlib.pyplot as plt
 #plt.plot(data['ProductName'],data['Price'])
+plt.subplot(1,2,1)
 plt.scatter(x,data1,color='blue',label = '1st')
 plt.scatter(x,data2,color='Red',label = '2nd')
 plt.legend(['Prive vs Unit'])
 plt.legend()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+plt.subplot(1,2,2)
+ypoints = np.array([3, 8, 1, 10, 5, 7])
+plt.plot(ypoints,color='green',marker = '+',linestyle=(0,(3,10,1,10,)),)
 plt.show()
