bdb4a9695d81a823b9692e03ec5e8c99ee50bc9c
Pranjal939 <pranjaljotangiya191@gmail.com>
2026-02-09 11:06:34 +0530

    commit

---
+++ task1.py
@@ -0,0 +1,19 @@
+
+my_int = 10
+my_float = 3.14
+my_string = "Hello, world!"
+my_list = [1, 2, 3, 4]
+
+print(my_int, type(my_int))
+print(my_float, type(my_float))
+print(my_string, type(my_string))
+print(my_list, type(my_list))
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive")
+elif num < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
